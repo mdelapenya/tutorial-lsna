561651ab918f55346259fe9996a565e80b0b6423
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-11-19 02:04:21 +0100

    Add a JS function to create a user

--- ui/assets/scripts/create.js
+++ ui/assets/scripts/create.js
@@ -12,5 +12,33 @@ if (WeDeploy.auth(address.auth).currentUser) {document.location.href = './chat.h
 var create = document.querySelector('.create');
 
 // Paste Create User code below //
-
+function userCreate() {
+    WeDeploy
+      .auth(address.auth)
+      .createUser({
+        name: create.name.value,
+        email: create.email.value,
+        password: create.password.value,
+        color: 'color-' + Math.floor(Math.random() * 19)
+      })
+      .then(function() {
+        create.submit.disabled = true;
+        create.submit.innerText = 'Loading...';
+  
+        WeDeploy
+          .auth(address.auth)
+          .signInWithEmailAndPassword(create.email.value, create.password.value)
+          .then(function() {
+            document.location.href = './chat.html';
+          })
+          .catch(function() {
+            alert('Sign-in failed.');
+          });
+      })
+      .catch(function() {
+        create.submit.disabled = false;
+        create.submit.innerText = 'Create Account';
+        alert('Sign-up failed.');
+      })
+  };
 // Paste Create User code above //
